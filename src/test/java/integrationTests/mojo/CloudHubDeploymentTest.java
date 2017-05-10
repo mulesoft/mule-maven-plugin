7a145873effc7a70b8b5afca2965e6b44e559185
William <william.comp.ita@gmail.com>
2017-05-10 15:53:07 -0300

    MMP-139 Create undeploy test, delete older in Groovy and refactor clo… (#27)
    * MMP-139 Create undeploy test, delete older in Groovy and refactor cloudhub deployment verifier
    
    * MMP-139 Changes due to PR

--- src/test/java/integrationTests/mojo/CloudHubDeploymentTest.java
+++ src/test/java/integrationTests/mojo/CloudHubDeploymentTest.java
@@ -7,7 +7,7 @@
 package integrationTests.mojo;
 
 import integrationTests.ProjectFactory;
-import integrationTests.mojo.verifier.CloudHubVerifier;
+import integrationTests.mojo.verifier.CloudHubDeploymentVerifier;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.junit.Before;
@@ -17,20 +17,23 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class CloudHubDeploymentTest {
 
+  private static final String MULE_UNDEPLOY = "mule:undeploy";
+  private static final String APPLICATION_NAME = "empty-mule-deploy-cloudhub-project";
   private static Logger log;
   private static Verifier verifier;
   private static File projectBaseDirectory;
   private static ProjectFactory builder;
   private static final String INSTALL = "install";
   private static final String MULE_DEPLOY = "mule:deploy";
-  private static final CloudHubVerifier cloudHubVerifier = new CloudHubVerifier();
+  private static final CloudHubDeploymentVerifier cloudHubDeploymentVerifier = new CloudHubDeploymentVerifier();
 
   public void initializeContext() throws IOException, VerificationException {
     builder = new ProjectFactory();
-    projectBaseDirectory = builder.createProjectBaseDir("empty-mule-deploy-cloudhub-project", this.getClass());
+    projectBaseDirectory = builder.createProjectBaseDir(APPLICATION_NAME, this.getClass());
     verifier = new Verifier(projectBaseDirectory.getAbsolutePath());
     verifier.addCliOption("-Dproject.basedir=" + projectBaseDirectory.getAbsolutePath());
     verifier.setMavenDebug(true);
@@ -42,16 +45,31 @@ public class CloudHubDeploymentTest {
     log.info("Initializing context...");
     initializeContext();
     verifier.executeGoal(INSTALL);
-  }
-
-  @Test
-  public void testCloudHubDeploy() throws VerificationException {
     verifier.setEnvironmentVariable("username", System.getProperty("username"));
     verifier.setEnvironmentVariable("password", System.getProperty("password"));
     verifier.setEnvironmentVariable("environment", "Production");
     verifier.setEnvironmentVariable("mule.version", System.getProperty("mule.version"));
+  }
+
+  @Test
+  public void testCloudHubDeploy() throws VerificationException, InterruptedException, TimeoutException {
+    log.info("Executing mule:deploy goal...");
     verifier.executeGoal(MULE_DEPLOY);
-    cloudHubVerifier.verify();
+    cloudHubDeploymentVerifier.verifyIsDeployed();
+    log.info("Application " + APPLICATION_NAME + " successfully deployed to CloudHub.");
+    verifier.verifyErrorFreeLog();
+  }
+
+  @Test
+  public void testCloudHubUndeploy() throws VerificationException, InterruptedException, TimeoutException {
+    log.info("Executing mule:deploy goal...");
+    verifier.executeGoal(MULE_DEPLOY);
+    cloudHubDeploymentVerifier.validateStatus(cloudHubDeploymentVerifier.DEPLOYED_STATUS);
+    log.info("Application successfully deployed to CloudHub.");
+    log.info("Executing mule:undeploy goal...");
+    verifier.executeGoal(MULE_UNDEPLOY);
+    cloudHubDeploymentVerifier.verifyIsUndeployed();
+    log.info("Application " + APPLICATION_NAME + " successfully undeployed from CloudHub.");
     verifier.verifyErrorFreeLog();
   }
 }
--- src/test/java/integrationTests/mojo/verifier/CloudHubDeploymentVerifier.java
+++ src/test/java/integrationTests/mojo/verifier/CloudHubDeploymentVerifier.java
@@ -8,6 +8,7 @@ package integrationTests.mojo.verifier;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -16,12 +17,15 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import static javax.xml.ws.BindingProvider.USERNAME_PROPERTY;
+import org.slf4j.Logger;
+
+import java.util.concurrent.TimeoutException;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-public class CloudHubVerifier {
+public class CloudHubDeploymentVerifier {
 
   private static final String URI = "https://anypoint.mulesoft.com";
   private static final String ME = "/accounts/api/me";
@@ -33,7 +37,7 @@ public class CloudHubVerifier {
   private static final String ORG_ID_HEADER = "X-ANYPNT-ORG-ID";
   private static final ClientBuilder clientBuilder = ClientBuilder.newBuilder();
   private static final Client client = clientBuilder.newClient();
-  private static final int ATTEMPTS = 300;
+  private static final int ATTEMPTS = 10;
   private static final String DATA = "data";
   private static final String ENVIRONMENT_NAME_KEY = "name";
   private static final String BEARER_FIELD = "bearer ";
@@ -45,39 +49,81 @@ public class CloudHubVerifier {
   private static final String USERNAME_ENVIRONMENT_VARIABLE = "username";
   private static final String PASSWORD_ENVIRONMENT_VARIABLE = "password";
   private static final String ACCESS_TOKEN_KEY = "access_token";
+  private static final String APPLICATIONS = "/cloudhub/api/applications";
+  private static final String APPLICATION_NAME = "maven-plugin-cloudhub-deploy-test";
+  private static final long SLEEP_TIME = 30000;
+  public static final String DEPLOYED_STATUS = "STARTED";
+  public static final String UNDEPLOYED_STATUS = "UNDEPLOYED";
+  private static final String STATUS_JSON_KEY = "status";
   private WebTarget target;
   private String response;
   private JSONObject responseJson;
   private IDs ids = new IDs();
   private String bearerToken;
+  private Logger log = LoggerFactory.getLogger(this.getClass());
 
-  public void verify() {
+
+  public void verifyIsDeployed() throws InterruptedException, TimeoutException {
+    loginAndGetNecessaryIds();
+    assertThat("Application was not deployed", validateStatus(DEPLOYED_STATUS), is(true));
+    deleteApplication();
+  }
+
+  public boolean validateStatus(String status) throws InterruptedException, TimeoutException {
+    log.info("Checking application " + APPLICATION_NAME + " for status " + status + "...");
+    int repeat = ATTEMPTS;
+    boolean keepValidating = false;
+    while (repeat > 0 && !keepValidating) {
+      JSONObject application = getApplication(getApplications());
+      keepValidating = !isExpectedStatus(status, application);
+      if (keepValidating) {
+        Thread.sleep(SLEEP_TIME);
+      }
+      repeat--;
+    }
+    if (repeat == 0 && !keepValidating) {
+      throw new TimeoutException("Validating status " + status + " for application " + APPLICATION_NAME
+          + " has exceed the maximum number of attempts.");
+    }
+    return keepValidating;
+  }
+
+  private boolean isExpectedStatus(String status, JSONObject application) {
+    if (DEPLOYED_STATUS.equals(status) && application != null) {
+      return status.equals(application.getString(STATUS_JSON_KEY));
+    }
+    if (UNDEPLOYED_STATUS.equals(status)) {
+      return application == null || UNDEPLOYED_STATUS.equals(application.getString(STATUS_JSON_KEY));
+    }
+    return false;
+  }
+
+  public void verifyIsUndeployed() throws InterruptedException, TimeoutException {
+    loginAndGetNecessaryIds();
+    assertThat("Application was not undeployed", validateStatus(UNDEPLOYED_STATUS), is(true));
+    deleteApplication();
+  }
+
+  private void loginAndGetNecessaryIds() {
     login();
     obtainOrganizationId();
     obtainEnvironmentId();
-    boolean wasDeleted = deleteApplication();
-    assertThat("Application was not deployed", wasDeleted, is(true));
   }
 
-  private boolean deleteApplication() {
-    boolean hasReceivedDeleteConfirmation = false;
+  private void deleteApplication() {
     target = client.target(URI).path(APPLICATION);
-    Response deleteResponse =
-        target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, BEARER_FIELD + bearerToken)
-            .header(ENV_ID_HEADER, ids.getEnvironmentId()).header(ORG_ID_HEADER, ids.getOrganizationId()).delete();
-    hasReceivedDeleteConfirmation = deleteResponse.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL;
-    int repeat = ATTEMPTS;
-    boolean deleted = false;
-    while (repeat > 0 && !deleted) {
-      target = ClientBuilder.newClient().target(URI).path(APPLICATION);
-      Response response = target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, BEARER_FIELD + bearerToken)
-          .header(ENV_ID_HEADER, ids.getEnvironmentId()).header(ORG_ID_HEADER, ids.getOrganizationId()).get();
-      deleted = response.getStatus() == Response.Status.NOT_FOUND.getStatusCode();
-    }
-    return hasReceivedDeleteConfirmation && deleted;
+    Response deleteResponse = delete(target);
+    assertThat("Application was not deleted", deleteResponse.getStatusInfo().getFamily() == Response.Status.Family.SUCCESSFUL);
+  }
+
+  private Response delete(WebTarget target) {
+    log.info("Deleting application " + target.getUri());
+    return target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, BEARER_FIELD + bearerToken)
+        .header(ENV_ID_HEADER, ids.getEnvironmentId()).header(ORG_ID_HEADER, ids.getOrganizationId()).delete();
   }
 
   private void obtainEnvironmentId() {
+    log.info("Getting environment ID...");
     target = client.target(URI).path(String.format(ENVIRONMENTS_PATH, ids.getOrganizationId()));
     response =
         target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, BEARER_FIELD + bearerToken)
@@ -94,6 +140,7 @@ public class CloudHubVerifier {
   }
 
   private void obtainOrganizationId() {
+    log.info("Getting organization ID...");
     target = client.target(URI).path(ME);
     response =
         target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, BEARER_FIELD + bearerToken)
@@ -103,6 +150,7 @@ public class CloudHubVerifier {
   }
 
   private void login() {
+    log.info("Trying to log in to Anypoint...");
     WebTarget target = client.target(URI).path(LOGIN);;
     String username = System.getProperty(USERNAME_ENVIRONMENT_VARIABLE);
     String password = System.getProperty(PASSWORD_ENVIRONMENT_VARIABLE);
@@ -110,7 +158,30 @@ public class CloudHubVerifier {
     String response = target.request(MediaType.APPLICATION_JSON_TYPE).post(requestJson, String.class);
     JSONObject responseJson = new JSONObject(response);
     bearerToken = responseJson.getString(ACCESS_TOKEN_KEY);
+    log.info("Login is successful, bearer token is " + bearerToken);
   }
+
+  public JSONObject getApplication(JSONArray applications) {
+    log.info("Trying to find application " + APPLICATION_NAME + " within all existent applications in CloudHub account...");
+    for (int i = 0; i < applications.length(); ++i) {
+      if (APPLICATION_NAME.equals(applications.getJSONObject(i).getString("domain"))) {
+        log.info("The application " + APPLICATION_NAME + " was found.");
+        return applications.getJSONObject(i);
+      }
+    }
+    log.info("The application " + APPLICATION_NAME + " could not be found.");
+    return null;
+  }
+
+  public JSONArray getApplications() {
+    log.info("Fetching all applications from CloudHub account...");
+    target = ClientBuilder.newClient().target(URI).path(APPLICATIONS);
+    response = target.request(MediaType.APPLICATION_JSON_TYPE).header(AUTHORIZATION_HEADER, "bearer " + bearerToken)
+        .header(ENV_ID_HEADER, ids.getEnvironmentId()).header(ORG_ID_HEADER, ids.getOrganizationId()).get(String.class);
+    log.info("Applications fetched.");
+    return new JSONArray(response);
+  }
+
 
   private class IDs {
 

38201e375a9afe8f0b18153952c5d199808b7e6b
damiansima <damiansima@gmail.com>
2017-10-10 19:52:52 +0200

    MMP-265, ensure only .xml files are considered for config when popula… (#118)
    * MMP-265, ensure only .xml files are considered for config when populating mule artifact json
    
    MMP-265, filter hidden files in muleartifactContentResolver
    
    * MMP-265, ensure test passes on win

--- mule-packager/src/test/java/org/mule/tools/api/packager/sources/MuleArtifactContentResolverTest.java
+++ mule-packager/src/test/java/org/mule/tools/api/packager/sources/MuleArtifactContentResolverTest.java
@@ -10,20 +10,23 @@
 
 package org.mule.tools.api.packager.sources;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
+
 import org.mule.tools.api.packager.structure.ProjectStructure;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.equalTo;
 
 public class MuleArtifactContentResolverTest {
 
@@ -35,10 +38,14 @@ public class MuleArtifactContentResolverTest {
   private static final String CONFIG_2 = "config2.xml";
   private static final String CONFIG_3_LOCATION = "lolo/lulu";
   private static final String CONFIG_3 = "config3.xml";
+
+  private static final String COMMON_FILE = "aFile.txt";
+
   private static final String JAVA_FOLDER_LOCATION = "src/main/java";
   private static final String MULE_FOLDER_LOCATION = "src/main/mule";
   private static final String MUNIT_FOLDER_LOCATION = "src/test/munit";
   private static final String RESOURCES_FOLDER_LOCATION = "src/main/resources";
+  public static final String HIDDEN_FILE = ".hiddenFile";
   @Rule
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
@@ -96,15 +103,26 @@ public class MuleArtifactContentResolverTest {
     File jar2 = new File(resourcesFolder, JAR_2);
     File jar3Folder = new File(resourcesFolder, JAR_3_LOCATION);
     File jar3 = new File(jar3Folder, JAR_3);
+
+    File hiddenFile = new File(munitFolder, HIDDEN_FILE);
+    // Ensure hidden fin in win based systems
+    if (System.getProperty("os.name").toLowerCase().equals("win")) {
+      Files.setAttribute(hiddenFile.toPath(), "dos:hidden", true);
+    }
+
     jar1.createNewFile();
     jar2.createNewFile();
     jar3Folder.mkdirs();
     jar3.createNewFile();
+    hiddenFile.createNewFile();
 
     List<String> actualExportedResources = resolver.getExportedResources();
 
     assertThat("Exported resources does not contain all expected elements", actualExportedResources,
                containsInAnyOrder(JAR_1, JAR_2, JAR_3_LOCATION + File.separator + JAR_3));
+
+    assertThat("Configs contain an unexpected elements", actualExportedResources.contains(hiddenFile), is(false));
+
     assertThat("Exported resources contains more elements than expected", actualExportedResources.size(), equalTo(3));
   }
 
@@ -114,15 +132,22 @@ public class MuleArtifactContentResolverTest {
     File config2 = new File(muleFolder, CONFIG_2);
     File config3Folder = new File(muleFolder, CONFIG_3_LOCATION);
     File config3 = new File(config3Folder, CONFIG_3);
+    File commonFile = new File(muleFolder, COMMON_FILE);
+
     config1.createNewFile();
     config2.createNewFile();
     config3Folder.mkdirs();
     config3.createNewFile();
+    commonFile.createNewFile();
+
 
     List<String> actualConfigs = resolver.getConfigs();
 
     assertThat("Configs does not contain all expected elements", actualConfigs,
                containsInAnyOrder(CONFIG_1, CONFIG_2, CONFIG_3_LOCATION + File.separator + CONFIG_3));
+
+    assertThat("Configs contain an unexpected elements", actualConfigs.contains(COMMON_FILE), is(false));
+
     assertThat("Configs contains more elements than expected", actualConfigs.size(), equalTo(3));
   }
 
@@ -132,15 +157,22 @@ public class MuleArtifactContentResolverTest {
     File config2 = new File(munitFolder, CONFIG_2);
     File config3Folder = new File(munitFolder, CONFIG_3_LOCATION);
     File config3 = new File(config3Folder, CONFIG_3);
+    File commonFile = new File(munitFolder, COMMON_FILE);
+
     config1.createNewFile();
     config2.createNewFile();
     config3Folder.mkdirs();
     config3.createNewFile();
+    commonFile.createNewFile();
+
     resolver = new MuleArtifactContentResolver(new ProjectStructure(temporaryFolder.getRoot().toPath(), true));
     List<String> actualConfigs = resolver.getTestConfigs();
 
     assertThat("Configs does not contain all expected elements", actualConfigs,
                containsInAnyOrder(CONFIG_1, CONFIG_2, CONFIG_3_LOCATION + File.separator + CONFIG_3));
+
+    assertThat("Configs contain an unexpected elements", actualConfigs.contains(COMMON_FILE), is(false));
+
     assertThat("Configs contains more elements than expected", actualConfigs.size(), equalTo(3));
   }
 }

0eefb82945c4c32caa3373266b33df1a57bef287
William Pinho <william.comp.ita@gmail.com>
2018-01-23 18:42:00 -0300

    MMP-329 Add IT and refactor to work with JDK 7

---
+++ mule-artifact-tools-it/mule-deployer-it/src/test/java/integration/test/mojo/agent/ApplicationAgentDeploymentTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package integration.test.mojo.agent;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ApplicationAgentDeploymentTest extends AgentDeploymentTest {
+
+  private static final String APPLICATION = "empty-mule-deploy-application-agent-project";
+
+  public ApplicationAgentDeploymentTest() {
+    super(APPLICATION);
+  }
+
+  @Override
+  public void assertDeployment() {
+    assertThat("Failed to deploy: " + APPLICATION, standaloneEnvironment.isDeployed(APPLICATION), is(true));
+  }
+}
---
+++ mule-artifact-tools-it/mule-deployer-it/src/test/java/integration/test/util/FileUtils.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package integration.test.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.DosFileAttributeView;
+
+public class FileUtils {
+
+  public static void markAsReadOnly(File file) throws IOException {
+    boolean succeeded = file.setReadOnly();
+    // We need to add this extra logic due to a bug in windows (JDK-6728842)
+    if (!succeeded) {
+      Path filePath = file.toPath();
+      FileStore fileStore = Files.getFileStore(filePath);
+      if (fileStore.supportsFileAttributeView(DosFileAttributeView.class)) {
+        DosFileAttributeView fileAttributeView = Files.getFileAttributeView(filePath, DosFileAttributeView.class);
+        fileAttributeView.setReadOnly(true);
+      }
+    }
+  }
+
+  public static void checkReadOnly(File file) throws IOException {
+    // This logic check if the file attribute was mark as read only because File#setReadOnly is platform dependent (JDK-6728842)
+    Path repositoryPath = file.toPath();
+    FileStore fileStore = Files.getFileStore(repositoryPath);
+    if (fileStore.supportsFileAttributeView(DosFileAttributeView.class)) {
+      DosFileAttributeView fileAttributeView = Files.getFileAttributeView(repositoryPath, DosFileAttributeView.class);
+      if (fileAttributeView.readAttributes().isReadOnly()) {
+        throw new IOException("File is not writable");
+      }
+    }
+  }
+
+  public static void copyDirectoryRecursively(File origin, File destination, CopyOption... copyOption) throws IOException {
+    if (!destination.exists()) {
+      destination.mkdir();
+    }
+    for (String child : origin.list()) {
+      copyDirectory(new File(origin, child), new File(destination, child), copyOption);
+    }
+  }
+
+  private static void copyDirectory(File origin, File destination, CopyOption... copyOption) throws IOException {
+    if (origin.isDirectory()) {
+      copyDirectoryRecursively(origin, destination, copyOption);
+    } else {
+      Files.copy(origin.toPath(), destination.toPath(), copyOption);
+    }
+  }
+}
+

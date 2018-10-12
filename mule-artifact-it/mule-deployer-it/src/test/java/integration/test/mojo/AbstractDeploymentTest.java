10dd8feda8b9e4e6da02cc6d4e77c70e392263f5
Julian Pascual <julian.pascual@mulesoft.com>
2018-10-12 11:48:13 -0300

    Fix deployment integration tests

--- mule-artifact-it/mule-deployer-it/src/test/java/integration/test/mojo/AbstractDeploymentTest.java
+++ mule-artifact-it/mule-deployer-it/src/test/java/integration/test/mojo/AbstractDeploymentTest.java
@@ -27,7 +27,7 @@ import integration.test.util.StandaloneEnvironment;
 public abstract class AbstractDeploymentTest {
 
   private static final String MAVEN_OPTS = "MAVEN_OPTS";
-  private static final String DEFAULT_MULE_VERSION = "4.2.0-SNAPSHOT";
+  private static final String DEFAULT_MULE_VERSION = "4.1.4";
   private static final String MAVEN_OPTS_PROPERTY_KEY = "argLine";
 
   protected static final String DEPLOY_GOAL = "deploy";

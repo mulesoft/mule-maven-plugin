934b0878d9d0cc749f787a13717c34742749b560
julianpascualMuleSoft <julian.pascual@mulesoft.com>
2019-01-10 17:17:46 -0300

    MMP-433 Fix NPE when having distribution management section on POM

--- mule-maven-plugin/src/test/java/org/mule/tools/maven/mojo/deploy/AbstractMuleDeployerMojoTest.java
+++ mule-maven-plugin/src/test/java/org/mule/tools/maven/mojo/deploy/AbstractMuleDeployerMojoTest.java
@@ -12,15 +12,22 @@ package org.mule.tools.maven.mojo.deploy;
 
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.Build;
+import org.apache.maven.model.DeploymentRepository;
+import org.apache.maven.model.DistributionManagement;
+import org.apache.maven.model.Model;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.Settings;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.mule.tools.client.core.exception.DeploymentException;
+import org.mule.tools.model.anypoint.CloudHubDeployment;
 import org.mule.tools.model.standalone.ClusterDeployment;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Mockito.*;
@@ -30,8 +37,8 @@ import java.io.File;
 public class AbstractMuleDeployerMojoTest {
 
   private AbstractMuleDeployerMojo mojoSpy;
-  private MavenProject mavenProjectMock;
-
+  private MavenProject projectMock;
+  private MavenSession sessionMock;
 
   @Rule
   public ExpectedException expectedException = ExpectedException.none();
@@ -39,14 +46,32 @@ public class AbstractMuleDeployerMojoTest {
   @Before
   public void setUp() {
     mojoSpy = spy(AbstractMuleDeployerMojo.class);
-    mavenProjectMock = mock(MavenProject.class);
+
+    sessionMock = mock(MavenSession.class);
+    mojoSpy.setSession(sessionMock);
+    when(sessionMock.getGoals()).thenReturn(newArrayList("deploy"));
+
+    Build buildMock = mock(Build.class);
+    when(buildMock.getDirectory()).thenReturn("");
+
+    projectMock = mock(MavenProject.class);
+    when(projectMock.getBuild()).thenReturn(buildMock);
+    when(projectMock.getPackaging()).thenReturn("mule-application");
+    when(projectMock.getModel()).thenReturn(mock(Model.class));
+    when(projectMock.getGroupId()).thenReturn("groupId");
+    when(projectMock.getArtifactId()).thenReturn("artifactId");
+    when(projectMock.getVersion()).thenReturn("1.0.0");
+    mojoSpy.setProject(projectMock);
+
+    File projectBaseFolder = new File(".");
+    mojoSpy.setProjectBaseFolder(projectBaseFolder);
+
+    mojoSpy.initMojo();
   }
 
   // In this test, we simulate the behavior of what is inject by plexus when one deployment
   // configuration is defined, i.e., all but one deployment configuration are null.
   // The resolved configuration should be the non-null.
-  // TODO re enable
-  @Ignore
   @Test
   public void setDeploymentOneDeploymentNotNullTest() throws DeploymentException {
     mojoSpy.setAgentDeployment(null);
@@ -55,41 +80,47 @@ public class AbstractMuleDeployerMojoTest {
     mojoSpy.setStandaloneDeployment(null);
 
     ClusterDeployment clusterDeploymentMock = mock(ClusterDeployment.class);
-    doNothing().when(clusterDeploymentMock).setDefaultValues(mavenProjectMock);
+    doNothing().when(clusterDeploymentMock).setDefaultValues(projectMock);
     mojoSpy.setClusterDeployment(clusterDeploymentMock);
-
-    MavenSession sessionMock = mock(MavenSession.class);
-    mojoSpy.setSession(sessionMock);
-
-    Build buildMock = mock(Build.class);
-    when(buildMock.getDirectory()).thenReturn("");
-
-    MavenProject projectMock = mock(MavenProject.class);
-    when(projectMock.getBuild()).thenReturn(buildMock);
-    mojoSpy.setProject(projectMock);
-
-    File projectBaseFolder = new File(".");
-    mojoSpy.setProjectBaseFolder(projectBaseFolder);
-
-    mojoSpy.initMojo();
-    // mojoSpy.setDeployment();
 
     assertThat("The resolved deployment is not the expected", mojoSpy.getDeploymentConfiguration(),
                equalTo(clusterDeploymentMock));
   }
 
-  // TODO re enable
-  @Ignore
+  @Test
+  public void setDeploymentOneDeploymentAndDistributionManagementTest() throws DeploymentException {
+    mojoSpy.setAgentDeployment(null);
+    mojoSpy.setArmDeployment(null);
+    mojoSpy.setStandaloneDeployment(null);
+    mojoSpy.setClusterDeployment(null);
+
+    CloudHubDeployment cloudHubDeploymentMock = mock(CloudHubDeployment.class);
+    doNothing().when(cloudHubDeploymentMock).setDefaultValues(projectMock);
+    mojoSpy.setCloudHubDeployment(cloudHubDeploymentMock);
+
+    DistributionManagement distributionManagementMock = mock(DistributionManagement.class);
+    DeploymentRepository deploymentRepositoryMock = mock(DeploymentRepository.class);
+    Settings settingsMock = mock(Settings.class);
+    when(deploymentRepositoryMock.getUrl()).thenReturn("https://maven.anypoint.mulesoft.com/api/v1/organizations/f8b03602-3b92-4bac-ada1-1ddd017ef5f4/maven");
+    when(distributionManagementMock.getRepository()).thenReturn(deploymentRepositoryMock);
+    when(settingsMock.getServer(any())).thenReturn(mock(Server.class));
+    when(projectMock.getDistributionManagement()).thenReturn(distributionManagementMock);
+    when(sessionMock.getSettings()).thenReturn(settingsMock);
+
+    assertThat("The resolved deployment is not the expected", mojoSpy.getDeploymentConfiguration(),
+            equalTo(cloudHubDeploymentMock));
+  }
+
   @Test
   public void setDeploymentAllDeploymentNullTest() throws DeploymentException {
     expectedException.expect(DeploymentException.class);
-    expectedException.expectMessage("Please define one deployment configuration");
+    expectedException.expectMessage("No deployment configuration was defined. Aborting.");
     mojoSpy.setAgentDeployment(null);
     mojoSpy.setArmDeployment(null);
     mojoSpy.setCloudHubDeployment(null);
     mojoSpy.setStandaloneDeployment(null);
     mojoSpy.setClusterDeployment(null);
 
-    // mojoSpy.setDeployment();
+    mojoSpy.getDeploymentConfiguration();
   }
 }

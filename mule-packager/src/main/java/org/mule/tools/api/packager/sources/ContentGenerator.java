51d3b5db74d4534b503c47931aff938ed9b8c9c8
egrave <eduardograve3@gmail.com>
2022-07-27 12:33:33 -0300

    W-11327063: Packaging not working properly having... (#599) (#605)
    * W-11327063: Packaging not working properly having...
    
    a mule-lib that uses a parent pom that is referenced using a parent's
    property as version.
    
    The pom generated by maven-jar-plugin in the same scenario has the
    version copied from the parent when it's installed. We need to do this
    for relativepath cases.
    
    * removing commas

--- mule-packager/src/main/java/org/mule/tools/api/packager/sources/ContentGenerator.java
+++ mule-packager/src/main/java/org/mule/tools/api/packager/sources/ContentGenerator.java
@@ -15,13 +15,28 @@ import static org.mule.tools.api.packager.structure.FolderNames.MAVEN;
 import static org.mule.tools.api.packager.structure.FolderNames.META_INF;
 import static org.mule.tools.api.packager.structure.PackagerFiles.POM_PROPERTIES;
 import static org.mule.tools.api.packager.structure.PackagerFiles.POM_XML;
-import org.mule.tools.api.packager.ProjectInformation;
 
+import org.apache.maven.model.Parent;
+import org.mule.tools.api.packager.ProjectInformation;
+import org.mule.tools.api.util.XmlFactoryUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 /**
  * Generates the required content for each of the mandatory folders of a mule package
@@ -29,11 +44,13 @@ import java.nio.file.StandardCopyOption;
 public abstract class ContentGenerator {
 
   protected final ProjectInformation projectInformation;
+  private Parent parent;
 
-  public ContentGenerator(ProjectInformation projectInformation) {
+  public ContentGenerator(ProjectInformation projectInformation, Parent parent) {
     checkArgument(projectInformation.getProjectBaseFolder().toFile().exists(), "Project base folder should exist");
     checkArgument(projectInformation.getBuildDirectory().toFile().exists(), "Project build folder should exist");
     this.projectInformation = projectInformation;
+    this.parent = parent;
   }
 
   /**
@@ -51,8 +68,31 @@ public abstract class ContentGenerator {
         projectInformation.getBuildDirectory().resolve(META_INF.value()).resolve(MAVEN.value())
             .resolve(projectInformation.getGroupId()).resolve(projectInformation.getArtifactId());
     String destinationFileName = originPath.getFileName().toString();
-
-    copyFile(originPath, destinationPath, destinationFileName);
+    try {
+      if (parent != null && parent.getRelativePath() != null) {
+        javax.xml.parsers.DocumentBuilderFactory factory = XmlFactoryUtils.createSecureDocumentBuilderFactory();
+        Document pomFile = factory.newDocumentBuilder().parse(originPath.toFile());
+        if (pomFile != null && pomFile.getDocumentElement() != null) {
+          Node parentNode = pomFile.getElementsByTagName("parent").item(0);
+          NodeList parentNodes = parentNode.getChildNodes();
+          for (int i = 0; i < parentNodes.getLength(); i++) {
+            if (parentNodes.item(i).getNodeName().equals("version")) {
+              parentNodes.item(i).getFirstChild().setNodeValue(parent.getVersion());
+            }
+          }
+        }
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(pomFile);
+        FileWriter writer = new FileWriter(destinationPath.resolve(destinationFileName).toFile());
+        StreamResult result = new StreamResult(writer);
+        transformer.transform(source, result);
+      } else {
+        copyFile(originPath, destinationPath, destinationFileName);
+      }
+    } catch (SAXException | IOException | ParserConfigurationException | TransformerException | NullPointerException e) {
+      copyFile(originPath, destinationPath, destinationFileName);
+    }
   }
 
   public static void checkPathExist(Path path) {

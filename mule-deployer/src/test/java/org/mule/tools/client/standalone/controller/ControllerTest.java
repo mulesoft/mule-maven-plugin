a5e5e34b088e16c10878ca4f8d55fb9a79eb0dfc
aguozden <aguozden@mulesoft.com>
2025-01-22 23:43:43 -0300

    W-17506523: mule-deploryer coverage to 43

--- mule-deployer/src/test/java/org/mule/tools/client/standalone/controller/ControllerTest.java
+++ mule-deployer/src/test/java/org/mule/tools/client/standalone/controller/ControllerTest.java
@@ -7,12 +7,33 @@
 package org.mule.tools.client.standalone.controller;
 
 import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.mockito.ArgumentCaptor;
+import org.mockito.MockedStatic;
+import org.mule.tools.client.standalone.exception.MuleControllerException;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ControllerTest {
 
@@ -35,6 +56,104 @@ public class ControllerTest {
     controller.addConfProperty("2");
     controller.deploy(temporaryFolder.toPath().resolve("mule-app").toString());
     controller.deployDomain(temporaryFolder.toPath().resolve("mule-app").toString());
+    controller.deployDomain(temporaryFolder.toPath().resolve("conf").toFile().getAbsolutePath().toString());
     controller.addLibrary(temporaryFolder.toPath().resolve("lib.jar").toFile());
   }
+
+  private static class XController extends Controller {
+
+    public XController(AbstractOSController osSpecificController, String muleHome) {
+      super(osSpecificController, muleHome);
+      this.libsDir = mock(File.class);
+    }
+
+    void resetController() {
+      reset(this.libsDir);
+    }
+  }
+
+  private final AbstractOSController osController = mock(AbstractOSController.class);
+  private final XController controller = new XController(osController, UUID.randomUUID().toString());
+
+  @BeforeEach
+  void setUp() {
+    reset(osController);
+    controller.resetController();
+  }
+
+  @Test
+  void getMuleBinTest() {
+    String value = UUID.randomUUID().toString();
+
+    when(osController.getMuleBin()).thenReturn(value);
+
+    assertThat(controller.getMuleBin()).isEqualTo(value);
+  }
+
+  @Test
+  void startTest() {
+    // VALUES
+    String value = UUID.randomUUID().toString();
+
+    //CONFIG
+    ArgumentCaptor<String[]> args = ArgumentCaptor.forClass(String[].class);
+    doNothing().when(osController).start(args.capture());
+
+    //EXEC
+    controller.start(value);
+
+    //VALIDATION
+    verify(osController, times(1)).start(any());
+    assertThat(args.getValue()).hasSize(1).containsExactly(value);
+  }
+
+  @ParameterizedTest
+  @MethodSource("addLibraryTestValues")
+  void addLibraryTest(File jar, String message, boolean canWrite) {
+    when(controller.libsDir.canWrite()).thenReturn(canWrite);
+
+    if (!canWrite) {
+      assertThatThrownBy(() -> controller.addLibrary(jar))
+          .isInstanceOf(MuleControllerException.class)
+          .hasMessageContaining(message);
+    } else {
+      try (MockedStatic<FileUtils> fileUtils = mockStatic(FileUtils.class)) {
+        fileUtils.when(() -> FileUtils.copyFileToDirectory(any(), any())).then(invocationOnMock -> null);
+        controller.addLibrary(jar);
+
+        fileUtils.verify(() -> FileUtils.copyFileToDirectory(any(), any()));
+
+        fileUtils.when(() -> FileUtils.copyFileToDirectory(any(), any())).thenThrow(new IOException());
+        assertThatThrownBy(() -> controller.addLibrary(jar))
+            .isInstanceOf(MuleControllerException.class);
+      }
+    }
+  }
+
+  static Stream<Arguments> addLibraryTestValues() {
+    File notFound = mock(File.class);
+    when(notFound.exists()).thenReturn(false);
+
+    File notExt = mock(File.class);
+    when(notExt.exists()).thenReturn(true);
+    when(notExt.getAbsolutePath()).thenReturn("anyName.txt");
+
+    File readOnly = mock(File.class);
+    when(readOnly.exists()).thenReturn(true);
+    when(readOnly.getAbsolutePath()).thenReturn("library.jar");
+    when(readOnly.canRead()).thenReturn(false);
+
+    File jarFile = mock(File.class);
+    when(jarFile.exists()).thenReturn(true);
+    when(jarFile.getAbsolutePath()).thenReturn("library.jar");
+    when(jarFile.canRead()).thenReturn(true);
+
+    return Stream.of(
+                     Arguments.of(notFound, "Jar file does not exist", false),
+                     Arguments.of(notExt, "don't have .jar extension.", false),
+                     Arguments.of(readOnly, "Cannot read jar file", false),
+                     Arguments.of(jarFile, "Cannot write on lib dir", false),
+                     Arguments.of(jarFile, null, true));
+  }
+
 }

937209da74324b6e7a0db271f110efdc8e5556b1
Jorge Rodriguez <rodriguez.jorge@salesforce.com>
2025-03-26 20:16:45 -0300

    IT

--- mule-artifact-it/mule-packager-it/src/test/java/integration/test/mojo/CompileMojoTest.java
+++ mule-artifact-it/mule-packager-it/src/test/java/integration/test/mojo/CompileMojoTest.java
@@ -15,7 +15,9 @@ import java.io.IOException;
 import org.apache.maven.shared.verifier.VerificationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
+@Disabled
 class CompileMojoTest extends MojoTest implements SettingsConfigurator {
 
   private static final String GOAL = "compile";
